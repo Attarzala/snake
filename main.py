c9d2a62dc815b1873459ca4c8aac4489f6bdb3f5
Fredrik Andréen <fredrik.andreen@diadrom.se>
2020-10-06 23:17:42 +0200

    Commit for comparing sentdex and mine

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
 import pygame
-
-
+import time
 pygame.init()
 
 width = 800
@@ -9,12 +7,34 @@ height = 600
 black = (0, 0, 0)
 white = (255, 255, 255)
 
-car_width = 73
+ball_width = 120
 
 clock = pygame.time.Clock()
+pygame.display.set_caption('A bit Racey')
 screen = pygame.display.set_mode((width, height))
 
-ball = pygame.image.load("intro_ball.png")
+ball = pygame.image.load('intro_ball.png')
+
+
+def text_objects(text, font):
+    text_surface = font.render(text, True, black)
+    return text_surface, text_surface.get_rect()
+
+
+def message_display(text):
+    text_type = pygame.font.SysFont(None, size=70)
+    text_surf, text_rect = text_objects(text, text_type)
+    text_rect.center = (width/2, height/2)
+    screen.blit(text_surf, text_rect)
+    pygame.display.update()
+
+    time.sleep(3)
+
+    game_loop()
+
+
+def fail():
+    message_display("You sack")
 
 
 def update_ball(x, y):
@@ -27,32 +47,35 @@ def game_loop():
     x_change = 0
     y_change = 0
     error = False
+
     while not error:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit()
-
+                error = True
             if event.type == pygame.KEYDOWN:
-                if pygame.K_DOWN:
-                    y_change = -5
-                if pygame.K_UP:
+                if event.key == pygame.K_DOWN:
                     y_change = 5
-                if pygame.K_LEFT:
-                    x_change = -5
-                if pygame.K_RIGHT:
-                    x_change = 5
-
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_DOWN or event.key == pygame.K_UP:
                     x_change = 0
+                if event.key == pygame.K_UP:
+                    y_change = -5
+                    x_change = 0
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
                     y_change = 0
-
+                if event.key == pygame.K_RIGHT:
+                    x_change = 5
+                    y_change = 0
         y += y_change
         x += x_change
 
-        screen.fill(black)
+        screen.fill(white)
         update_ball(x, y)
+
+        if x > width - ball_width or x < 0 or y > height - ball_width or y < 0:
+            fail()
+
+
 
         pygame.display.update()
         clock.tick(60)
