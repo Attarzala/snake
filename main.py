2f7f27a26cda4b5e75ebefed98c0ed71ee09de76
Fredrik Andréen <fredrik.andreen@diadrom.se>
2020-10-06 23:17:42 +0200

    Commit for comparing sentdex and mine

--- main.py
+++ main.py
@@ -1,58 +1,58 @@
-import sys
 import pygame
-
 
 pygame.init()
 
-width = 800
-height = 600
+display_width = 800
+display_height = 600
+
 black = (0, 0, 0)
 white = (255, 255, 255)
+red = (255, 0, 0)
 
 car_width = 73
 
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((width, height))
 
-ball = pygame.image.load("intro_ball.png")
+carImg = pygame.image.load('intro_ball.png')
 
 
-def update_ball(x, y):
-    screen.blit(ball, (x, y))
+def car(x, y):
+    gameDisplay.blit(carImg, (x, y))
 
 
 def game_loop():
-    x = (width * 0.45)
-    y = (height * 0.8)
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+
     x_change = 0
-    y_change = 0
-    error = False
-    while not error:
+
+    gameExit = False
+
+    while not gameExit:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit()
+                gameExit = True
 
             if event.type == pygame.KEYDOWN:
-                if pygame.K_DOWN:
-                    y_change = -5
-                if pygame.K_UP:
-                    y_change = 5
-                if pygame.K_LEFT:
+                if event.key == pygame.K_LEFT:
                     x_change = -5
-                if pygame.K_RIGHT:
+                if event.key == pygame.K_RIGHT:
                     x_change = 5
 
             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_DOWN or event.key == pygame.K_UP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
-                    y_change = 0
 
-        y += y_change
         x += x_change
 
-        screen.fill(black)
-        update_ball(x, y)
+        gameDisplay.fill(white)
+        car(x, y)
+
+        if x > display_width - car_width or x < 0:
+            game_exit = True
 
         pygame.display.update()
         clock.tick(60)
